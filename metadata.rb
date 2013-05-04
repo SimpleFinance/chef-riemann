be400a0299cdc04e2349a0ee685c2d6e9d5d3dda
Jake Davis <jake.davis5989@gmail.com>
2013-05-04 16:36:41 +0000

    A bunch of goodies -- gitignore, license, Vagrantfile, etc

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'riemann'
+maintainer       'Simple Finance'
+maintainer_email 'ops@simple.com'
+description      'Installs/Configures Riemann'
+version          '1.0.0'
+
+depends 'java'
+depends 'runit'
+
