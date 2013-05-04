eeb11cdee492e21ba0f9f1543b37365d934b4316
Jake Davis <jake.davis5989@gmail.com>
2013-05-04 23:36:47 +0000

    Attrs to support default recipe and some thoughts on what'll be needed later

---
+++ attributes/default.rb
@@ -0,0 +1,23 @@
+# attributes/default.rb
+
+default[:riemann][:version] = '0.2.1'
+case node[:platform]
+when 'ubuntu' || 'debian'
+  default[:riemann][:package] = "riemann_#{node[:riemann][:version]}_all.deb"
+when 'fedora'
+  default[:riemann][:package] = "riemann_#{node[:riemann][:version]}-1.noarch.rpm"
+else
+  default[:riemann][:package] = "riemann_#{node[:riemann][:version]}.tar.bz2"
+end
+default[:riemann][:package_url] = "http://aphyr.com/riemann/#{node[:riemann][:package]}"
+
+default[:riemann][:user][:name] = 'riemann'
+default[:riemann][:user][:home] = '/home/riemann'
+default[:riemann][:user][:shell] = '/bin/bash'
+
+default[:riemann][:dashboard][:enable] = true
+default[:riemann][:dashboard][:directory] = '/opt/riemann-dash'
+
+default[:riemann][:health][:enable] = false
+default[:riemann][:riak][:enable] = false
+
